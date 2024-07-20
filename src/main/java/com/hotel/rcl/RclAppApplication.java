8c276246beff98b3751c603949f11a9f5f9ada4b
Edem AMEGBETO <amegbetoedem@gmail.com>
2024-07-20 08:21:23 +0200

    Initialisation du projet

---
+++ src/main/java/com/hotel/rcl/RclAppApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.rcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RclAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RclAppApplication.class, args);
+	}
+
+}
